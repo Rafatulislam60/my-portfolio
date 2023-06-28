8abd1837256da95ea8c48fb3938932341ac3296c
Rafatulislam60 <rafatulislam67@gmail.com>
2023-06-28 14:47:12 +0600

    add home and about page

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+import Social from "./Social";
+import "./home.css";
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home_container container grid">
+        <div className="home_content grid">
+          <Social></Social>
+
+          <div className="home_img"></div>
+
+          <Data />
+        </div>
+
+        <ScrollDown />
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,32 @@
+const Social = () => {
+  return (
+    <div className="home_social">
+      <a
+        href="https://www.facebook.com/rafatul.islam.545"
+        className="home_social-icon"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="uil uil-facebook"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/rafatul-islam-07363219b"
+        className="home_social-icon"
+        target="_blank" rel="noreferrer"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a
+        href="https://github.com/Rafatulislam60"
+        className="home_social-icon"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;

67a7e852ad58cfa51012985e62ba0c77f6a29d8f
Rafatulislam60 <rafatulislam67@gmail.com>
2023-07-02 13:46:33 +0600

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Service from "./components/service/Service";
@@ -17,6 +18,7 @@ const App = () => {
         <Skills />
         <Service />
         <Testimonials />
+        <Contact />
       </main>
     </>
   );
